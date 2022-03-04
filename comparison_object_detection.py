4e7c129fd31bed0780b25b6e0e719a4f64d80422
Pongsiri Klaewkla <69916211+pongsiri20907@users.noreply.github.com>
2022-03-04 23:36:13 +0700

    Create mqtt
    - pip install paho-mqtt
    - install mosquitto on Window 10

--- comparison_object_detection.py
+++ comparison_object_detection.py
@@ -2,12 +2,16 @@ import cv2
 import numpy as np
 
 # Import image
-image_original = cv2.imread('./image/original.png')
-image_compare = cv2.imread('./image/2.png')
+image_original = cv2.imread('./image/3.png')
+image_compare = cv2.imread('./image/1.png')
 
 # Preprocessing
 image_original = cv2.cvtColor(image_original, cv2.COLOR_RGB2GRAY)
 image_compare = cv2.cvtColor(image_compare, cv2.COLOR_RGB2GRAY)
+
+# Convert to binary
+_,image_original = cv2.threshold(image_original, 0, 60, cv2.THRESH_BINARY)
+_,image_compare = cv2.threshold(image_compare, 0, 60, cv2.THRESH_BINARY)
 
 # Resize image
 image_original = cv2.resize(image_original, (250,250))
@@ -16,13 +20,12 @@ image_compare = cv2.resize(image_compare, (250,250))
 # Compare image
 if image_original.shape == image_compare.shape:
     image_difference = cv2.subtract(image_original, image_compare)
+    print(image_difference)
 else:
     print("Shape not equal !")
 
-# Convert to binary
-_,image_original = cv2.threshold(image_original, 25, 30, cv2.THRESH_BINARY)
-_,image_compare = cv2.threshold(image_compare, 25, 50, cv2.THRESH_BINARY)
-_,image_difference = cv2.threshold(image_difference, 20, 150, cv2.THRESH_BINARY)
+# Create line
+cv2.line(image_difference,(0,125),(250,125),(255,0,0),1)
 
 # Show image
 cv2.imshow("Original", image_original)
---
+++ mqtt.py
@@ -0,0 +1,19 @@
+from http import client
+import paho.mqtt.client as paho
+from random import randrange, uniform
+import time
+import sys
+
+client = paho.Client("python1")
+
+if client.connect("localhost", 1883, 60) != 0:
+    print("Cannot connect to Mqtt Broker !")
+    sys.exit(-1)
+
+while True:
+    client.publish("test", "HELLO NODE RED !", 0)
+    time.sleep(3)
+
+
+
+
